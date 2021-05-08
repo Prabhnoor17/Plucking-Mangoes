15d759d0cbf97a7496f8134c1c408a35bf3a67ac
Prabhnoor17 <75075937+Prabhnoor17@users.noreply.github.com>
2021-05-08 19:41:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,9 +28,13 @@ function setup() {
 
 	ground = new Ground(400,680,800,20);
 
-	stone = new Stone(150,500,40);
+	stone = new Stone(150,500,20);
 
 	slingShot = new Slingshot(stone.body, {x:150,y:500})
+
+	mango1 = new Mango(600, 300, 20);
+	mango2 = new Mango(550, 300, 40);
+	
 
 
 
@@ -51,8 +55,11 @@ function draw() {
   stone.display();
   slingShot.display();
 
-  
-  
+  mango1.display();
+  mango2.display();
+
+  collission(mango1,stone);
+  collission(mango2,stone);
  
 }
 
@@ -64,3 +71,19 @@ function mouseReleased(){
 	slingShot.fly();
 }
 
+function collission(mango,stone){
+	var posMango = mango.body.position
+    var posStone = stone.body.position
+	var MR = mango.body.circleRadius
+	var SR = stone.body.circleRadius
+
+	if(posMango.x - posStone.x < MR+SR && 
+		posStone.x - posMango.x < MR+SR &&
+		posMango.y - posStone.y < MR + SR &&
+		posStone.y - posMango.y < MR+SR){
+
+			Matter.Body.setStatic(mango.body, false);
+
+	}
+	
+}
